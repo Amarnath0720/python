9f4b3f8db987f21cba906c733b4294a551af9857
Amarnath0720 <amarnathgaddam09@gmail.com>
2024-09-23 15:15:38 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,18 @@
+#height=int(input("enter height:"))
+#weight=int(input("enter some weight:"))
+#if height >=90:
+    #if weight >=40:
+        #print("will donate")
+#else:
+    #print("we cnt")
+#print()
+
+#i=1
+#while i<=10:
+    #print(i)
+    #if (i==7):
+        #break
+    #i +=1
+
+for w in range(0,31,2):
+        print(w)
